f113807a3cc2420ac6d90495863c3d1476ef4d87
김원형 <ansun667@naver.com>
2022-08-22 01:51:55 +0000

    version2.3

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,37 @@
 import styled from "styled-components";
 import React from 'react';
-import Circle from './Circle';
+import {useState} from 'react';
+
+const Container = styled.div`
+		background-color: ${(props)=>props.theme.bgColor};
+	`;
+const H1 = styled.h1`
+	color: ${(props)=>props.theme.textColor};
+	`;
 
 function App(){
 	return (
+		<Container>
+			<H1>protected</H1>
+		</Container>
+	);
+	/*const [value,setValue] = useState<string>("");
+	const onChange = (e:React.FormEvent<HTMLInputElement>)=>{
+		const {
+			currentTarget: {value},
+		} = e;
+		setValue(value);
+	}
+	const onSubmit = (e:React.FormEvent<HTMLFormElement>)=>{
+		e.preventDefault();
+	}
+	return (
 		<div>
-			<Circle bgColor="tomato"/>
-			<Circle bgColor="teal" borderColor="red" text="im here"/>
+			<form onSubmit={onSubmit}>
+				<input value={value} onChange={onChange} type="text" placeholder="username"/>
+				<button>Log in</button>
+			</form>
 		</div>);
+		*/
 }
 export default App;
