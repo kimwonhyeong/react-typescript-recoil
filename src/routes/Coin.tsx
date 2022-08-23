e0fc18fdbb314fcb9bd0d4d58c07d38a36100ec6
김원형 <ansun667@naver.com>
2022-08-23 14:24:35 +0000

    version3.1.1

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,4 +1,8 @@
+import {useParams} from "react-router"
+
+
 function Coin(){
+	const { coinId } = useParams<{coinId:string}>(); //구조 분해 할당 문법
 	return <h1>Coin</h1>;
 }
 export default Coin;
--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,11 +1,7 @@
 import {useParams} from "react-router"
 
-interface RouteParams{
-	coinId: string;
-}
+
 function Coins(){
-	const { coinId } = useParams<>(); //구조 분해 할당 문법
-	
-	return <h1>Coin: {coinId}</h1>;
+	return <h1>coins</h1>
 }
 export default Coins;
