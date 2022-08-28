aca0391281b14bdcb5e731abe94ee5689255080a
김원형 <ansun667@naver.com>
2022-08-28 09:00:43 +0000

    version3.2

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,8 +1,43 @@
-import {useParams} from "react-router"
-
-
+import {useLocation,useParams} from "react-router";
+import styled from "styled-components";
+import {useState} from 'react';
+const Container = styled.div`
+	padding: 0px 20px;
+	max-width: 480px;
+	margin: 0 auto;
+`;
+const Header = styled.header`
+	height: 10vh;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`;
+const Title = styled.h1`
+	font-size: 48px;
+	color:${props => props.theme.accentColor};
+`;
+const Loader = styled.span`
+	text-align: center;
+	display: block;
+`;
+interface Params {
+	coinId: string;
+}
+interface RouteState{
+	name: string;
+}
 function Coin(){
-	const { coinId } = useParams<{coinId:string}>(); //구조 분해 할당 문법
-	return <h1>Coin</h1>;
+	const [loading, setLoading] = useState(true);
+	const { coinId } = useParams<Params>(); //구조 분해 할당 문법
+	const {state} = useLocation<RouteState>();
+	//위 문장이 제너럴의 활용이다. 입력값과 출력값의 타입을 any라 설정했다해도 입력값과 출력값이 같은지 검사할 수 있다.
+	return (
+		<Container>
+			<Header>
+				<Title>{ state?.name || "Loading"}</Title>
+			</Header>
+			{loading ? <Loader>Loading...</Loader>:null}
+		</Container>
+		);
 }
 export default Coin;
--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,7 +1,105 @@
-import {useParams} from "react-router"
+import styled from "styled-components";
+import {Link} from "react-router-dom";
+import {useState, useEffect} from "react";
 
+//styled-components
+const Container = styled.div`
+	padding: 0px 20px;
+	max-width: 480px;
+	margin: 0 auto;
+`;
+const Header = styled.header`
+	height: 10vh;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`;
+const CoinsList = styled.ul``;
+const Coin = styled.li`
+	display:flex;
+	background-color: white;
+	color: ${props => props.theme.bgColor};
+	padding: 10px;
+	height:70px;
+	margin-bottom: 10px;
+	border-radius: 15px;
+	font-size:25px;
+	a{	
+		display:flex;
+		align-items: center;
+		transition: color 0.2s ease-in;
+		padding:20px;
+	}
+	&:hover{
+		a{
+			color: ${(props)=>props.theme.accentColor};
+		}
+	}
+`;
+const Title = styled.h1`
+	font-size: 48px;
+	color:${props => props.theme.accentColor};
+`;
+const Loader = styled.span`
+	text-align: center;
+	display: block;
+`;
+const Img = styled.img`
+	width:35px;
+	height:35px;
+	margin-right:10px;
+`;
+//인터페이스
+interface CoinInterface{
+	id: string;
+	name: string;
+	symbol: string;
+	rank: number;
+	is_new: boolean;
+	is_active: boolean;
+	type: string;
+}
 
+//메인 함수
 function Coins(){
-	return <h1>coins</h1>
+	const [coins, setCoins] = useState<CoinInterface[]>([]);
+	const [loading, setLoading] = useState(true);
+	useEffect(()=>{
+		console.log(3);
+		(async()=>{
+			console.log(4);
+			const response = await fetch("https://api.coinpaprika.com/v1/coins");
+			console.log(5);
+			const json = await response.json();
+			setCoins(json.slice(0,100));
+			setLoading(false);
+		})();
+		console.log(loading);
+	},[])
+	console.log(coins);
+	console.log(loading);
+	return (
+		<Container>
+			<Header>
+				<Title>코인</Title>
+			</Header>
+			{loading ? <Loader>Loading...</Loader> :<CoinsList>
+				{coins.map((coin)=>(
+					<Coin key={coin.id}>
+						<Link
+							to={{
+								 pathname:`/${coin.id}`,
+								 state:{name: coin.name},
+							 }}>
+							<Img src={`https://coinicons-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`}/>
+							{coin.name} →
+							
+						</Link>
+					</Coin>)
+					)
+				}
+			</CoinsList>}
+		</Container>
+	);
 }
 export default Coins;
