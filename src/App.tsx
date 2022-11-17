71242590d4fa23090bee3fc6a58a814d47253bab
김원형 <ansun667@naver.com>
2022-11-17 01:01:18 +0000

    animation4

--- src/App.tsx
+++ src/App.tsx
@@ -12,50 +12,22 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: rgba(255, 255, 255, 0.2);
   border-radius: 20px;
+  background-color: white;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
   display: flex;
   flex-wrap: wrap;
   justify-content: space-around;
   align-content: space-around;
 `;
-
-const A = styled(motion.div)`
-	box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-	width: 70px;
-	height: 70px;
-	background-color: white;
-	border-radius: 100%;
-`;
- const myVars = {
-	 start: {opacity: 0, scale: 0.5},
- 	 end: {opacity: 1,
-		   scale:1,
-		   transition:{
-			   type: "spring",
-			   bounce:0.5,
-			   when: "beforeChildren",
-			   staggerChildren: 0.2,
-		   }}
+ const boxVariants = {
+	 click: {scale:1,borderRadius:"100px"},
+ 	 hover: {scale:1.5, rotateZ:90},
  };
-const miniVars = {
-	start: {opacity: 0, y: 10},
-	end: {opacity: 1,
-		  y:0,
-		  transition:{
-			  when: "afterChildren"
-		 }}
-};
 function App(){
 	return (
 		<Wrapper>
-			<Box variants={myVars} initial="start" animate="end">
-				<A variants={miniVars}></A>
-				<A variants={miniVars}></A>
-				<A variants={miniVars}></A>
-				<A variants={miniVars}></A>
-			</Box>
+			<Box variants={boxVariants} whileHover="hover" whileTap="click"></Box>
 		</Wrapper>
 	);
 }
