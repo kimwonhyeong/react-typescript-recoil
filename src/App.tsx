c2df9176a310af8b82708a240c9dfbcf13a3f1eb
김원형 <ansun667@naver.com>
2022-11-17 02:15:15 +0000

    animation5

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { motion } from "framer-motion";
 import styled from "styled-components";
+import {useRef} from "react";
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -18,16 +19,39 @@ const Box = styled(motion.div)`
   display: flex;
   flex-wrap: wrap;
   justify-content: space-around;
-  align-content: space-around;
+  align-items: space-around;
 `;
+const BiggerBox = styled.div`
+	width:600px;
+	height:600px;
+	background-color: rgba(255, 255, 255, 0.2);
+	border-radius: 40px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	overflow: hidden;
+`;
+
  const boxVariants = {
 	 click: {scale:1,borderRadius:"100px"},
  	 hover: {scale:1.5, rotateZ:90},
+	 drag: {backgroundColor:"rgb(52, 152, 219)"},
  };
 function App(){
+	const biggerBoxRef = useRef<HTMLDivElement>(null);
 	return (
 		<Wrapper>
-			<Box variants={boxVariants} whileHover="hover" whileTap="click"></Box>
+			<BiggerBox ref={biggerBoxRef}>
+				<Box drag
+					 dragConstraints={biggerBoxRef}
+					 variants={boxVariants}
+					 whileDrag="drag"
+					 whileHover="hover"
+					 whileTap="click"
+					 dragSnapToOrigin
+					 dragElastic={0.5} //인력의 정도. 근데 이거는 문서에서 도저히 못 찾겠던데.
+					 ></Box>
+			</BiggerBox>
 		</Wrapper>
 	);
 }
